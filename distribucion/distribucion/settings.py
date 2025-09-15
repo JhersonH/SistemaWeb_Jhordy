21ac06e53b22c343868224837f3454bb40059995
Jherson <jhunnior1514@gmail.com>
2025-09-14 23:49:49 -0500

    version two

--- distribucion/distribucion/settings.py
+++ distribucion/distribucion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4u+jj16^yv@+(1u6%b*g#p@#h9v71ejr*s%@=16b7w^o76xy5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Kerim.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
